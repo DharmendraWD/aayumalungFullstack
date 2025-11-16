afe79061a4959026724fa8fc95b565f912af45fb
Dharmendra <dharmendrathakur.dev@gmail.com>
2025-11-16 15:20:20 +0545

    changes

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,24 @@ import { fileURLToPath } from "url";
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(
-  cors({
-    origin: 'http://localhost:3000', // your Next.js frontend
-    credentials: true,               // allow cookies or Authorization headers
-  })
-)
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://aayumalungfullstack-production.up.railway.app'
+];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin like mobile apps or curl requests
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
+
 
 
 // middleware 
